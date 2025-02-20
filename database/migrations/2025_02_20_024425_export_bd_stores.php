88cfc0a4f5e71d074d1705893332f4cdfcc4407c
Sebastian Contreras <sebasfac2017@gmail.com>
2025-02-20 00:17:50 -0300

    stores procedures

---
+++ database/migrations/2025_02_20_024425_export_bd_stores.php
@@ -0,0 +1,398 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //        
+        $sql = "
+ 
+
+DROP PROCEDURE IF EXISTS bsp_activar_usuario ;
+
+CREATE  PROCEDURE bsp_activar_usuario(pIdUsuario smallint)
+SALIR:BEGIN
+	/*
+		Permite cambiar el estado del usuario a A: Activo siempre y cuando no esté activo ya. Devuelve OK o el mensaje de error en Mensaje.
+	*/
+    
+    	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,'error' as Response,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    
+    -- Controlar autor no dado de baja
+    IF EXISTS(SELECT IdUsuario FROM Usuarios WHERE IdUsuario = pIdUsuario 
+						AND EstadoUsuario = 'A') THEN
+		SELECT 'El usuario ya está Activo.' AS Mensaje,'error' as Response;
+        LEAVE SALIR;
+	END IF;
+
+	-- Da de baja
+    UPDATE Usuarios SET EstadoUsuario = 'A' WHERE IdUsuario = pIdUsuario;
+	
+    SELECT 'OK' AS Mensaje,'ok' as Response;
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_alta_usuario ;
+
+CREATE  PROCEDURE bsp_alta_usuario(pUsername varchar(20), pApellidos varchar(30), pNombres varchar(30), pFechaNacimiento date, pTelefono varchar(15), pEmail varchar(60), pContrasena char(32), pRol char(1)   )
+SALIR:BEGIN
+/*
+	Permite dar de alta un usuario controlando que el username ni el correo electronico esten registrados. 
+    Lo da de alta con estado A: Activa. Devuelve OK + Id o el mensaje de error en Mensaje.
+*/
+	DECLARE pIdUsuario int;
+    -- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,'error' as Response,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    -- Controla parámetros obligatorios
+	IF	pUsername = '' OR pUsername IS NULL OR
+		pApellidos = '' OR pApellidos IS NULL OR
+        pNombres = '' OR pNombres IS NULL OR
+        pContrasena = '' OR pContrasena IS NULL OR
+        pRol = '' OR pRol IS NULL OR
+        pEmail = '' OR pEmail IS NULL THEN
+		SELECT 'Faltan datos obligatorios.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla longitud de la contraseña
+    IF CHAR_LENGTH(COALESCE(pContrasena,'')) < 6 THEN
+		SELECT 'La contraseña tiene que ser mayor a 6.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un autor con la misma cuenta
+	IF EXISTS(SELECT Username FROM Usuarios WHERE Username = pUsername) THEN
+		SELECT 'Ya existe un usuario con ese nombre de cuenta.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un autor con la misma dirección de correo electrónico
+	IF EXISTS(SELECT Email FROM Usuarios WHERE Email = pEmail) THEN
+		SELECT 'Ya existe un usuario con esa dirección de correo electrónico.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    
+    -- COMIENZO TRANSACCION
+    START TRANSACTION;
+        INSERT INTO Usuarios
+(`IdUsuario`,`Username`,`Apellidos`,`Nombres`,`FechaNacimiento`,`Telefono`,`Email`,`Contrasena`,`FechaCreado`,`Rol`,`EstadoUsuario`) VALUES
+(0,pUsername,pApellidos,pNombres,pFechaNacimiento,pTelefono,pEmail,md5(pContrasena),now(),pRol,'A');
+		SET pIdUsuario = LAST_INSERT_ID();
+
+        SELECT 'OK' AS Mensaje,'ok' as Response, pIdUsuario AS Id;                       
+    COMMIT;
+
+
+-- Devuelve OK + Id o el mensaje de error en Mensaje
+-- Mensaje varchar(100), Id int
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_borra_usuario ;
+
+CREATE  PROCEDURE bsp_borra_usuario(pIdUsuario smallint)
+SALIR:BEGIN
+	/*
+		Permite borrar un usuario, solamente usado para limpiar base de datos y en produccion. 
+		Devuelve OK o el mensaje de error en Mensaje.
+    */
+    -- Manejo de error de la transacción
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		-- SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador' Mensaje,'error' as Response;
+        ROLLBACK;
+    END;
+
+    START TRANSACTION;
+		-- Borra usuario
+        DELETE FROM Usuarios WHERE IdUsuario = pIdUsuario;
+
+        SELECT 'OK' Mensaje,'ok' as Response;
+    COMMIT;
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_dame_usuario ;
+
+CREATE  PROCEDURE bsp_dame_usuario(pIdUsuario smallint)
+BEGIN
+	/*
+		Procedimiento que sirve para instanciar un usuario desde la base de datos.
+    */
+    SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    
+    SELECT	*, 'ok' as Response
+    FROM	Usuarios
+    WHERE	IdUsuario = pIdUsuario;
+    
+    SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_darbaja_usuario ;
+
+CREATE  PROCEDURE bsp_darbaja_usuario(pIdUsuario smallint)
+SALIR:BEGIN
+	/*
+		Permite cambiar el estado del usuario a B: Baja siempre y cuando no esté dada de baja. Devuelve OK o el mensaje de error en Mensaje.
+	*/
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,'error' as Response,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+        
+    -- Controlar autor no dado de baja
+    IF EXISTS(SELECT IdUsuario FROM Usuarios WHERE IdUsuario = pIdUsuario 
+						AND EstadoUsuario = 'B') THEN
+		SELECT 'El usuario ya está dado de baja.' AS Mensaje,'error' as Response;
+        LEAVE SALIR;
+	END IF;
+
+	-- Da de baja
+    UPDATE Usuarios SET EstadoUsuario = 'B' WHERE IdUsuario = pIdUsuario;
+	
+    SELECT 'OK' AS Mensaje,'ok' as Response;
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_listar_establecimiento ;
+
+CREATE  PROCEDURE bsp_listar_establecimiento(pIncluyeBajas char(1))
+BEGIN
+	/*
+	Permite listar los establecimiento registrados. Puede mostrar o no las inactivas (pIncluyeBajas: S: Si - N: No)
+*/
+	    SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+ 
+		SELECT		*
+		FROM		Establecimientos
+		WHERE		
+					(pIncluyeBajas = 'S' OR EstadoEstablecimiento = 'A')
+		ORDER BY IdEstablecimiento
+		;
+    
+		SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- {Campos de la Tabla Establecimientos}
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_listar_usuarios ;
+
+CREATE DEFINER=root@localhost PROCEDURE bsp_listar_usuarios(pIncluyeBajas char(1))
+BEGIN
+	/*
+	Permite listar los usuarios de sistema. Puede mostrar o no las inactivas (pIncluyeBajas: S: Si - N: No)
+*/
+	    SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+ 
+		SELECT		IdUsuario,
+Username,
+Apellidos,
+Nombres,
+FechaNacimiento,
+Telefono,
+Email,
+FechaCreado,
+Rol,
+EstadoUsuario
+		FROM		Usuarios
+		WHERE		
+					(pIncluyeBajas = 'S' OR EstadoUsuario = 'A')
+		ORDER BY IdUsuario
+		;
+    
+		SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- {Campos de la Tabla Usuarios}
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_modificar_perfil ;
+
+CREATE  PROCEDURE bsp_modificar_perfil(pIdUsuario smallint,pUsername varchar(20), pApellidos varchar(30), pNombres varchar(30), pFechaNacimiento date, pTelefono varchar(15), pEmail varchar(60), pContrasena char(32) )
+SALIR:BEGIN
+/*
+	Permite modificar el perfil usuario controlando que el username ni el correo electronico esten registrados., 
+    ademas que el mismo no este dado de Baja  Devuelve OK + Id o el mensaje de error en Mensaje.
+*/
+	DECLARE pIdUsuario int;
+    -- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    -- Controla parámetros obligatorios
+	IF	pUsername = '' OR pUsername IS NULL OR
+		pApellidos = '' OR pApellidos IS NULL OR
+        pNombres = '' OR pNombres IS NULL OR
+        pEmail = '' OR pEmail IS NULL THEN
+		SELECT 'Faltan datos obligatorios.' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla longitud de la contraseña
+    IF CHAR_LENGTH(COALESCE(pContrasena,'')) < 6 THEN
+		SELECT 'La contraseña tiene que ser mayor a 6.' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un autor con la misma cuenta
+	IF EXISTS(SELECT Username FROM Usuarios WHERE Username = pUsername) THEN
+		SELECT 'Ya existe un usuario con ese nombre de cuenta.' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un autor con la misma dirección de correo electrónico
+	IF EXISTS(SELECT Email FROM Usuarios WHERE Email = pEmail) THEN
+		SELECT 'Ya existe un usuario con esa dirección de correo electrónico.' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    
+    -- COMIENZO TRANSACCION
+    START TRANSACTION;
+
+ UPDATE Usuarios 
+SET 
+    Username = pUsername,
+    Apellidos = pApellidos,
+    Nombres = pNombres,
+    FechaNacimiento = pFechaNacimiento,
+    Telefono = pTelefono,
+    Email = pEmail,
+    Rol = pRol
+WHERE 
+    IdUsuario = pIdUsuario;
+        SELECT 'OK' AS Mensaje, pIdUsuario AS Id,'ok' as Response;                       
+		
+    COMMIT;
+
+
+-- Devuelve OK + Id o el mensaje de error en Mensaje
+-- Mensaje varchar(100), Id int
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_modificar_rol_usuario ;
+
+CREATE  PROCEDURE bsp_modificar_rol_usuario(pIdUsuario smallint, pRol char(1))
+SALIR:BEGIN
+/*
+	Permite cambiar el rol del usuario a A: Administrador, O:Organizados, V:Verificador siempre y cuando el usuario este activo. 
+    Devuelve OK o el mensaje de error en Mensaje.
+*/
+	DECLARE pIdUsuario int;
+    -- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    
+        -- Verificar que pRol sea uno de los valores permitidos
+    IF pRol NOT IN ('A', 'O', 'V') THEN
+        SELECT 'Rol no válido.' AS Mensaje, NULL AS Id;
+        LEAVE SALIR; -- Salir del procedimiento
+    END IF;
+
+    -- COMIENZO TRANSACCION
+    START TRANSACTION;
+ UPDATE Usuarios SET 
+		Rol = pRol
+        WHERE 
+		IdUsuario = pIdUsuario;
+        
+        SELECT 'OK' AS Mensaje, pIdUsuario AS Id;                       
+    COMMIT;
+
+
+-- Devuelve OK + Id o el mensaje de error en Mensaje
+-- Mensaje varchar(100), Id int
+END ;
+
+DROP PROCEDURE IF EXISTS bsp_modifica_perfil ;
+
+CREATE  PROCEDURE bsp_modifica_perfil(pIdUsuario smallint,pUsername varchar(20), pApellidos varchar(30), pNombres varchar(30), pFechaNacimiento date, pTelefono varchar(15), pEmail varchar(60), pContrasena char(32), pRol char(1)   )
+SALIR:BEGIN
+/*
+	Permite modificar el perfil usuario controlando que el username ni el correo electronico esten registrados., 
+    ademas que el mismo no este dado de Baja  Devuelve OK + Id o el mensaje de error en Mensaje.
+*/
+    -- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje, 'error' as Response,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    -- Controla parámetros obligatorios
+	IF	pUsername = '' OR pUsername IS NULL OR
+		pApellidos = '' OR pApellidos IS NULL OR
+        pNombres = '' OR pNombres IS NULL OR
+        pRol = '' OR pRol IS NULL OR
+        pEmail = '' OR pEmail IS NULL THEN
+		SELECT 'Faltan datos obligatorios.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+
+    -- Controla que no exista un autor con la misma cuenta
+	IF EXISTS(SELECT Username FROM Usuarios WHERE Username = pUsername AND pIdUsuario !=IdUsuario) THEN
+		SELECT 'Ya existe un usuario con ese nombre de cuenta.' AS Mensaje,'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un autor con la misma dirección de correo electrónico
+	IF EXISTS(SELECT Email FROM Usuarios WHERE Email = pEmail AND pIdUsuario !=IdUsuario) THEN
+		SELECT 'Ya existe un usuario con esa dirección de correo electrónico.' AS Mensaje, 'error' as Response, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    
+    -- COMIENZO TRANSACCION
+    START TRANSACTION;
+ UPDATE Usuarios 
+SET 
+    Username = pUsername,
+    Apellidos = pApellidos,
+    Nombres = pNombres,
+    FechaNacimiento = pFechaNacimiento,
+    Telefono = pTelefono,
+    Email = pEmail,
+    Rol = pRol
+WHERE 
+    IdUsuario = pIdUsuario;
+        SELECT 'OK' AS Mensaje, 'ok' as Response, pIdUsuario AS Id;                       
+		
+    COMMIT;
+
+
+-- Devuelve OK + Id o el mensaje de error en Mensaje
+-- Mensaje varchar(100), Id int
+END ;
+
+
+
+        ";
+        DB::unprepared($sql);
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
