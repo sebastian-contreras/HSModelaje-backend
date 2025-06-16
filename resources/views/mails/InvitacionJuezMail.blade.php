55626640e7f34bce37e7b3f2076f1bb6659a1c53
Sebastian Contreras <sebasfac2017@gmail.com>
2025-06-16 12:25:24 -0300

    arreglo app url mail juez

--- resources/views/mails/InvitacionJuezMail.blade.php
+++ resources/views/mails/InvitacionJuezMail.blade.php
@@ -16,7 +16,7 @@ Nos complace invitarte a ser parte de nuestro jurado para el evento **{{ $data['
 ---
 
 Para acceder a tu panel de jurado, por favor ingresa al siguiente enlace:
-@component('mail::button', ['url' => config('frontend_url') . '/voto-jurado/' . $data['juez']->Token, 'color' => 'primary'])
+@component('mail::button', ['url' => rtrim(config('app.frontend_url'), '/') . '/voto-jurado/' . $data['juez']->Token, 'color' => 'primary'])
 🎟️ Acceder como Jurado
 @endcomponent
 
