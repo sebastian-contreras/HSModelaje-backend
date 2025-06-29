4aac14fc60b8808d653905c0c7d7c129e5e41db1
Sebastian Contreras <sebasfac2017@gmail.com>
2025-06-28 21:52:39 -0300

    chore: Update Usuario.php

---
+++ app/Classes/Usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Classes;
+
+use DB;
+
+class Usuario
+{
+    public $IdUsuario;
+    public $Username;
+    public $Apellidos;
+    public $Nombres;
+    public $FechaNacimiento;
+    public $Telefono;
+    public $Email;
+    public $Contrasena;
+    public $Rol;
+    public $Activo;
+
+    public function __construct(array $data)
+    {
+        $this->IdUsuario = $data['IdUsuario'] ?? null;
+        $this->Username = $data['Username'] ?? null;
+        $this->Apellidos = $data['Apellidos'] ?? null;
+        $this->Nombres = $data['Nombres'] ?? null;
+        $this->FechaNacimiento = $data['FechaNacimiento'] ?? null;
+        $this->Telefono = $data['Telefono'] ?? null;
+        $this->Email = $data['Email'] ?? null;
+        $this->Contrasena = $data['Contrasena'] ?? null;
+        $this->Rol = $data['Rol'] ?? null;
+        $this->Activo = $data['Activo'] ?? null;
+    }
+
+
+    public function Dame()
+    {
+        return DB::select('CALL bsp_dame_usuario(?)', [$this->IdUsuario]);
+    }
+
+    public function Activar()
+    {
+        return DB::select('CALL bsp_activar_usuario(?)', [$this->IdUsuario]);
+    }
+
+    public function DarBaja()
+    {
+        return DB::select('CALL bsp_darbaja_usuario(?)', [$this->IdUsuario]);
+    }
+}
