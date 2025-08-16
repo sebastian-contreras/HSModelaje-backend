3d1a3efe50628fe074bb0dd8b7c71dda38170809
Sebastian Contreras <sebasfac2017@gmail.com>
2025-08-15 21:25:35 -0300

    fix gestr base gestor metricas

--- app/Services/GestorMetricas.php
+++ app/Services/GestorMetricas.php
@@ -5,14 +5,14 @@ namespace App\Services;
 use App\Classes\Metricas;
 use Illuminate\Support\Facades\DB;
 
-class GestorMetricas extends GestorBaseEvento
+class GestorMetricas extends GestorBase
 {
-    public function Listar($IdEvento, $pIncluyeBajas = 'N')
+    public function ListarEnEvento($IdEvento, $pIncluyeBajas = 'N')
     {
         return DB::select('CALL bsp_listar_metricas(?,?)', [$IdEvento, $pIncluyeBajas]);
     }
 
-    public function Buscar($Offset, $Cantidad, $IdEvento, $Metrica = null,$pIncluyeInactivos='N')
+    public function BuscarEnEvento($Offset, $Cantidad, $IdEvento, $Metrica = null, $pIncluyeInactivos = 'N')
     {
         return DB::select('CALL bsp_buscar_metricas(?,?,?,?,?)', [$IdEvento, $Metrica, $pIncluyeInactivos, $Offset, $Cantidad]);
     }
@@ -36,5 +36,25 @@ class GestorMetricas extends GestorBaseEvento
     public function Borra($IdMetrica)
     {
         return DB::select('CALL bsp_borra_metrica(?)', [$IdMetrica]);
+    }
+    /**
+     * @deprecated No implementado.
+     */
+    public function Listar($IdEntidad)
+    {
+        trigger_error(
+            'El método ' . __METHOD__ . ' está deprecado y no hace nada.',
+            E_USER_DEPRECATED
+        );
+    }
+    /**
+     * @deprecated No implementado.
+     */
+    public function Buscar($offset, $cantidad)
+    {
+        trigger_error(
+            'El método ' . __METHOD__ . ' está deprecado y no hace nada.',
+            E_USER_DEPRECATED
+        );
     }
 }
